7e39fde8b70d541866858b5b987bc13bf863608a
Carl Mitchell <carl.harris.mitchell@gmail.com>
2018-07-24 14:16:21 -0400

    Add some tests and comments for TimeUtilities

--- app/src/main/java/com/github/carlhmitchell/failsafealert/utilities/TimeUtilities.java
+++ app/src/main/java/com/github/carlhmitchell/failsafealert/utilities/TimeUtilities.java
@@ -3,12 +3,22 @@ package com.github.carlhmitchell.failsafealert.utilities;
 //??
 
 public class TimeUtilities {
+    /**
+     * Extracts the hour as an integer from a string formatted as "HH:MM"
+     * @param time is a string in the format "HH:MM"
+     * @return an integer, equal to HH
+     */
     public static int getHour(String time) {
         String[] pieces = time.split(":");
 
         return (Integer.parseInt(pieces[0]));
     }
 
+    /**
+     * Extracts the minute as an integer from a string formatted as "HH:MM"
+     * @param time is a string in the format "HH:MM"
+     * @return an integer, equal to MM
+     */
     public static int getMinute(String time) {
         String[] pieces = time.split(":");
 
--- app/src/test/java/com/github/carlhmitchell/failsafealert/utilities/TimeUtilitiesTest.java
+++ app/src/test/java/com/github/carlhmitchell/failsafealert/utilities/TimeUtilitiesTest.java
@@ -5,12 +5,15 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TimeUtilitiesTest {
+    private String time = "00:00";
 
     @Test
     public void getHour() {
+        assert 0 == TimeUtilities.getHour(time);
     }
 
     @Test
     public void getMinute() {
+        assert 0 == TimeUtilities.getMinute(time);
     }
 }
