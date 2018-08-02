6d9296a4c8722dfb423e22c7f208bf1dfb8dee15
Carl Mitchell <carl.harris.mitchell@gmail.com>
2018-08-02 19:12:01 -0400

    Make logging Async

--- app/src/main/java/com/github/carlhmitchell/failsafealert/utilities/SDLog.java
+++ app/src/main/java/com/github/carlhmitchell/failsafealert/utilities/SDLog.java
@@ -1,7 +1,9 @@
 package com.github.carlhmitchell.failsafealert.utilities;
 
+import android.os.AsyncTask;
 import android.os.Environment;
 import android.util.Log;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileWriter;
@@ -14,36 +16,48 @@ import static com.github.carlhmitchell.failsafealert.utilities.AppConstants.LOG_
 // Log() returns an int. I have had no reason to use these, but it's better to pass it through.
 @SuppressWarnings({"ResultOfMethodCallIgnored", "UnusedReturnValue"})
 public class SDLog {
-    private static final String LOG_FILE_NAME = "failsafe_alert.log";
-    private static final String LOG_LINES_NAME = "failsafe_alert_lines.log";
+    private static final String LOG_FILE_NAME = "failsafe_alert_logcat.log";
+    private static final String LOG_LINES_NAME = "failsafe_alert.log";
 
     /**
      * Runs logcat, outputting at the Verbose level. Saves output to a file on SD card.
      */
     public static void printLog() {
-        if (LOG_TO_SD) {
-            try {
-                String filename = Environment.getExternalStorageDirectory() + File.separator + LOG_FILE_NAME;
-                String command = "logcat -d *:V";
-                Process process = Runtime.getRuntime().exec(command);
+        ContinuousLogAsyncTask continuousLogAsyncTask = new ContinuousLogAsyncTask();
+        continuousLogAsyncTask.execute();
+    }
 
-                BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
-                String line;
+    /**
+     * Async task so that the UI thread doesn't lag while logging is turned on.
+     */
+    private static class ContinuousLogAsyncTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            if (LOG_TO_SD) {
                 try {
-                    File file = new File(filename);
-                    file.createNewFile();
-                    FileWriter writer = new FileWriter(file, true);
-                    while ((line = in.readLine()) != null) {
-                        writer.write(line + "\n");
+                    String filename = Environment.getExternalStorageDirectory() + File.separator + LOG_FILE_NAME;
+                    String command = "logcat -d *:V";
+                    Process process = Runtime.getRuntime().exec(command);
+
+                    BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                    String line;
+                    try {
+                        File file = new File(filename);
+                        file.createNewFile();
+                        FileWriter writer = new FileWriter(file, true);
+                        while ((line = in.readLine()) != null) {
+                            writer.write(line + "\n");
+                        }
+                        writer.flush();
+                        writer.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
-                    writer.flush();
-                    writer.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
             }
+            return null;
         }
     }
 
@@ -54,36 +68,24 @@ public class SDLog {
      * @param msg Message to print
      * @param type Type of msg (V, D, I, W, E, WTF)
      */
-    private static void logLine(String tag, String msg, String type) {
-        TimeFormatter timeFormatter = new TimeFormatter();
-        String time = timeFormatter.getFormattedTime();
-        if (LOG_TO_SD) {
-            try {
-                String filename = Environment.getExternalStorageDirectory() + File.separator + LOG_LINES_NAME;
-                File file = new File(filename);
-                file.createNewFile();
-                FileWriter writer = new FileWriter(file, true);
-                writer.write(time + ": " + type + "/" + tag + ": " + msg + "\n" );
-                writer.flush();
-                writer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+    private static void logLine(String tag, String msg, LogData.Type type) {
+        LogLineAsyncTask logAsyncTask = new LogLineAsyncTask();
+        LogData data = new LogData(tag, msg, type);
+        logAsyncTask.execute(data);
     }
 
     public static int v(String tag, String msg) {
-        logLine(tag, msg, "V");
+        logLine(tag, msg, LogData.Type.V);
         return Log.v(tag, msg);
     }
 
     public static int d(String tag, String msg) {
-        logLine(tag, msg, "D");
+        logLine(tag, msg, LogData.Type.D);
         return Log.d(tag, msg);
     }
 
     public static int i(String tag, String msg) {
-        logLine(tag, msg, "I");
+        logLine(tag, msg, LogData.Type.I);
         return Log.i(tag, msg);
     }
 
@@ -91,19 +93,76 @@ public class SDLog {
     @SuppressWarnings("unused")
     //Just because there are no Warning level log statements now doesn't mean there won't be later.
     public static int w(String tag, String msg) {
-        logLine(tag, msg, "W");
+        logLine(tag, msg, LogData.Type.W);
         return Log.w(tag, msg);
     }
 
     public static int e(String tag, String msg) {
-        logLine(tag, msg, "E");
+        logLine(tag, msg, LogData.Type.E);
         return Log.e(tag, msg);
     }
 
     @SuppressWarnings("unused")
     //Just because there are no WTF level log statements now doesn't mean there won't be later.
     public static int wtf(String tag, String msg) {
-        logLine(tag, msg, "WTF");
+        logLine(tag, msg, LogData.Type.WTF);
         return Log.wtf(tag, msg);
+    }
+
+    /**
+     * Async task so that the UI thread doesn't lag if logging is turned on.
+     */
+    private static class LogLineAsyncTask extends AsyncTask<LogData, Void, Void> {
+        @Override
+        protected Void doInBackground(LogData... params) {
+            LogData data = params[0];
+            String tag = data.getTag();
+            String msg = data.getMsg();
+            LogData.Type type = data.getLogType();
+            String time = TimeUtilities.getFormattedTime();
+            if (LOG_TO_SD) {
+                try {
+                    String filename = Environment.getExternalStorageDirectory() + File.separator + LOG_LINES_NAME;
+                    File file = new File(filename);
+                    file.createNewFile();
+                    FileWriter writer = new FileWriter(file, true);
+                    writer.write(time + ": " + type + "/" + tag + ": " + msg + "\n" );
+                    writer.flush();
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+    }
+
+    /**
+     * Class to handle the inputs to LogLineAsyncTask cleanly.
+     * Uses an enum type and two strings, and AsyncTasks can only take inputs of a single type.
+     */
+    static class LogData {
+        enum Type {V, D, I, W, E, WTF}
+        private String tag;
+        private String msg;
+        private Type logType;
+
+        LogData(String tag, String msg, Type logType) {
+            this.tag = tag;
+            this.msg = msg;
+            this.logType = logType;
+        }
+
+        public String getTag() {
+            return tag;
+        }
+
+        public String getMsg() {
+            return msg;
+        }
+
+        public Type getLogType() {
+            return logType;
+        }
     }
 }
