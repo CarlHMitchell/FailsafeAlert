ed24ba149159ceb20bb3285f7b570092012491f6
Carl Mitchell <carl.harris.mitchell@gmail.com>
2018-08-02 16:37:13 -0400

    Suppress false positive warnings. These constants are supposed to be available app-wide, to avoid needing magic numbers. Having them be public isn't a problem.

--- app/src/main/java/com/github/carlhmitchell/failsafealert/utilities/AppConstants.java
+++ app/src/main/java/com/github/carlhmitchell/failsafealert/utilities/AppConstants.java
@@ -2,12 +2,12 @@ package com.github.carlhmitchell.failsafealert.utilities;
 
 //N/A
 
+@SuppressWarnings("WeakerAccess") // These constants should be available app-wide.
 public class AppConstants {
     public static final int SWITCH_INACTIVE = 0;
     public static final int SWITCH_ACTIVE = 1;
     public static final String NOTIFICATION_CHANNEL_ID = "Debug Channel";
     public static final long MINUTE_MILLIS = 1000 * 60;
     public static final long WAKELOCK_TIMEOUT = MINUTE_MILLIS * 10;
-    //public static final String APP_DEBUG_TAG = "Failsafe Alert";
     public static final boolean LOG_TO_SD = true;
 }
