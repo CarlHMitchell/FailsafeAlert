40080ca4915531b3e2001639b6b82d228d4626d3
Carl Mitchell <carl.harris.mitchell@gmail.com>
2018-08-02 16:21:35 -0400

    Suppress some unneeded warnings, improve comments.

--- app/src/main/java/com/github/carlhmitchell/failsafealert/utilities/SDLog.java
+++ app/src/main/java/com/github/carlhmitchell/failsafealert/utilities/SDLog.java
@@ -11,13 +11,19 @@ import java.io.InputStreamReader;
 import static com.github.carlhmitchell.failsafealert.utilities.AppConstants.LOG_TO_SD;
 
 // File.createNewFile() is used simply to ensure that the log file exists.
-@SuppressWarnings("ResultOfMethodCallIgnored")
+// Log() returns an int. I have had no reason to use these, but it's better to pass it through.
+@SuppressWarnings({"ResultOfMethodCallIgnored", "UnusedReturnValue"})
 public class SDLog {
+    private static final String LOG_FILE_NAME = "failsafe_alert.log";
+    private static final String LOG_LINES_NAME = "failsafe_alert_lines.log";
 
+    /**
+     * Runs logcat, outputting at the Verbose level. Saves output to a file on SD card.
+     */
     public static void printLog() {
         if (LOG_TO_SD) {
             try {
-                String filename = Environment.getExternalStorageDirectory() + File.separator + "failsafe_alert.log";
+                String filename = Environment.getExternalStorageDirectory() + File.separator + LOG_FILE_NAME;
                 String command = "logcat -d *:V";
                 Process process = Runtime.getRuntime().exec(command);
 
@@ -45,19 +51,19 @@ public class SDLog {
     /**
      * Log a message to disk, if LOG_TO_SD constant is true.
      * @param tag Debug tag (name of calling class, etc)
-     * @param message Message to print
-     * @param type Type of message (V, D, I, W, E, WTF)
+     * @param msg Message to print
+     * @param type Type of msg (V, D, I, W, E, WTF)
      */
-    private static void logLine(String tag, String message, String type) {
+    private static void logLine(String tag, String msg, String type) {
         TimeFormatter timeFormatter = new TimeFormatter();
         String time = timeFormatter.getFormattedTime();
         if (LOG_TO_SD) {
             try {
-                String filename = Environment.getExternalStorageDirectory() + File.separator + "failsafe_alert_lines.log";
+                String filename = Environment.getExternalStorageDirectory() + File.separator + LOG_LINES_NAME;
                 File file = new File(filename);
                 file.createNewFile();
                 FileWriter writer = new FileWriter(file, true);
-                writer.write(time + ": " + type + "/" + tag + ": " + message + "\n" );
+                writer.write(time + ": " + type + "/" + tag + ": " + msg + "\n" );
                 writer.flush();
                 writer.close();
             } catch (IOException e) {
@@ -66,34 +72,38 @@ public class SDLog {
         }
     }
 
-    public static int v(String tag, String line) {
-        logLine(tag, line, "V");
-        return Log.v(tag, line);
+    public static int v(String tag, String msg) {
+        logLine(tag, msg, "V");
+        return Log.v(tag, msg);
     }
 
-    public static int d(String tag, String line) {
-        logLine(tag, line, "D");
-        return Log.d(tag, line);
+    public static int d(String tag, String msg) {
+        logLine(tag, msg, "D");
+        return Log.d(tag, msg);
     }
 
-    public static int i(String tag, String line) {
-        logLine(tag, line, "I");
-        return Log.i(tag, line);
+    public static int i(String tag, String msg) {
+        logLine(tag, msg, "I");
+        return Log.i(tag, msg);
     }
 
 
-    public static int w(String tag, String line) {
-        logLine(tag, line, "W");
-        return Log.w(tag, line);
+    @SuppressWarnings("unused")
+    //Just because there are no Warning level log statements now doesn't mean there won't be later.
+    public static int w(String tag, String msg) {
+        logLine(tag, msg, "W");
+        return Log.w(tag, msg);
     }
 
-    public static int e(String tag, String line) {
-        logLine(tag, line, "E");
-        return Log.e(tag, line);
+    public static int e(String tag, String msg) {
+        logLine(tag, msg, "E");
+        return Log.e(tag, msg);
     }
 
-    public static int wtf(String tag, String line) {
-        logLine(tag, line, "WTF");
-        return Log.wtf(tag, line);
+    @SuppressWarnings("unused")
+    //Just because there are no WTF level log statements now doesn't mean there won't be later.
+    public static int wtf(String tag, String msg) {
+        logLine(tag, msg, "WTF");
+        return Log.wtf(tag, msg);
     }
 }
